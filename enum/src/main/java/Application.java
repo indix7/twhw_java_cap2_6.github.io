754f4ef88bfc2520cec00ae0f3cce7f704fa44c4
maoxin <indix7@outlook.com>
2020-02-12 15:34:59 +0800

    20200212

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,15 @@
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        findAllFemale();
+    }
 
-  }
+    public static void findAllFemale() {
+        for (Student student : Student.values()) {
+            if (student.getGender().equals("Female")) {
+                System.out.println(student);
+            }
+        }
+    }
 
 }
---
+++ enum/src/main/java/Student.java
@@ -0,0 +1,35 @@
+public enum Student {
+    Linda("Linda", "Female"),
+    Bob("Bob", "Male"),
+    Solider("Solider", "Male"),
+    Cindy("Cindy", "Female");
+
+    private String name;
+    private String gender;
+
+    Student(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return ("姓名 ： " + this.name + "性别 ： " + this.gender);
+    }
+}
